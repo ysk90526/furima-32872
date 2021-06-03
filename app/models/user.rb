e15ea61597aa15107174eb712c0ed0317a0e265a
ysk90526 <y.h0526.0903@i.softbank.jp>
2021-06-03 17:23:15 +0900

    新規登録、ログイン、ログアウト機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+         validates :password, format: {with: VALID_PASSWORD_REGEX }
 
          validates :nickname, presence: true
-         validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
-         validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+         validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥々ー]+\z/}
+         validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥々ー]+\z/}
          validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
          validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
          validates :birthday, presence: true
