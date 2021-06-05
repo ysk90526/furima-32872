71550b2d37dc968069c26aaa88828ca4672de564
ysk90526 <y.h0526.0903@i.softbank.jp>
2021-06-05 22:06:03 +0900

    バリデーションとアソシエーションを記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :item_image
+  with_options presence: true do
+    validates :item_name
+    validates :item_description
+    validates :category_id
+    validates :item_status_id
+    validates :shipping_charge_id
+    validates :area_id
+    validates :day_id
+    validates :price, format: {with: /^[0-9]+$/ }
+  end
 end
