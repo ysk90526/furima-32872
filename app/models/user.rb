2b96ad5643313902b44e79e2cd283d7d16f673c5
ysk90526 <y.h0526.0903@i.softbank.jp>
2021-06-05 11:46:44 +0900

    Userモデルを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,14 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: 'Full-width characters' } do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width Kana' } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
     validates :birthday
   end
 end
