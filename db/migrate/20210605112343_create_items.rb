737fa94a1db368e6106ed944ac81a12f7d07e3ed
ysk90526 <y.h0526.0903@i.softbank.jp>
2021-06-05 20:54:38 +0900

    マイグレーションファイルにカラムを追加

--- db/migrate/20210605112343_create_items.rb
+++ db/migrate/20210605112343_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string :item_name,           null: false
+      t.text :item_description,      null: false
+      t.integer :category_id,        null: false
+      t.integer :item_status_id,     null: false
+      t.integer :shipping_charge_id, null: false
+      t.integer :area_id,            null: false
+      t.integer :day_id,             null: false
+      t.integer :price,              null: false
+      t.references :user,            foreign_key: true
       t.timestamps
     end
   end
