b66944e738aef048acef01ad50813f79b79d41fc
ysk90526 <y.h0526.0903@i.softbank.jp>
2021-06-12 11:30:07 +0900

    createのコントローラーを編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,5 +8,17 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name,:item_description,:category_id,:item_status_id,:shipping_charge_id,:area_id,:day_id,:price).merge(user_id: current_user.id)
   end
 end
