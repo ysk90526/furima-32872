90778afd88a983b590be257fd2f1bcffd44a46a7
ysk90526 <y.h0526.0903@i.softbank.jp>
2021-06-13 14:25:21 +0900

    FactoryBotを記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    item_image         {}
+    item_name          { 'アイテム' }
+    item_description   { 'アイテムの説明' }
+    category_id        {}
+    item_status_id     {}
+    shipping_charge_id {}
+    area_id            {}
+    day_id             {}
+    price              { '1000' }
   end
 end
