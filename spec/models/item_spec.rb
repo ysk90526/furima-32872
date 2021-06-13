75cae1ae39d0f54488cfc192248d6c9102f2fd9b
ysk90526 <y.h0526.0903@i.softbank.jp>
2021-06-13 14:04:49 +0900

    テストファイルを作成

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '商品出品登録' do
+    context '出品登録できるとき' do
+    end
+    context '出品登録できないとき' do
+    end
+  end
 end
