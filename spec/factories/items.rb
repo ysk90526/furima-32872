00fdba172abf4b3534ec5a23b754feadec1adb41
ysk90526 <y.h0526.0903@i.softbank.jp>
2021-06-19 23:05:07 +0900

    テストコードを修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :item do
-    item_image         {}
     item_name          { 'アイテム' }
     item_description   { 'アイテムの説明' }
-    category_id        {}
-    item_status_id     {}
-    shipping_charge_id {}
-    area_id            {}
-    day_id             {}
-    price              { '1000' }
+    category_id        {2}
+    item_status_id     {2}
+    shipping_charge_id {2}
+    area_id            {2}
+    day_id             {2}
+    price              {1000}
+    end
   end
 end
