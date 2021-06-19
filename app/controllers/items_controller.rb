dcad990b8225c497f7d7f608b1f37f20d7bbfb21
ysk90526 <y.h0526.0903@i.softbank.jp>
2021-06-19 19:38:43 +0900

    form_withとコントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:item_name,:item_description,:category_id,:item_status_id,:shipping_charge_id,:area_id,:day_id,:price).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_image,:item_name,:item_description,:category_id,:item_status_id,:shipping_charge_id,:area_id,:day_id,:price).merge(user_id: current_user.id)
   end
 end
