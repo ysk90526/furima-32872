10bce7bace7f652616bbbcbb6cd11c4027bba972
ysk90526 <y.h0526.0903@i.softbank.jp>
2021-06-20 14:32:13 +0900

    テストコードを修正2

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -8,6 +8,11 @@ FactoryBot.define do
     area_id            {2}
     day_id             {2}
     price              {1000}
+
+    association :user
+
+    after(:build) do |item|
+      item.item_image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
     end
   end
 end
