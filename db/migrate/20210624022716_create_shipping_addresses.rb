9e7ce4b6a7095610a6ff08bc909090560c65b1b8
ysk90526 <y.h0526.0903@i.softbank.jp>
2021-06-24 12:24:02 +0900

    マイグレーションを作成

--- db/migrate/20210624022716_create_shipping_addresses.rb
+++ db/migrate/20210624022716_create_shipping_addresses.rb
@@ -1,7 +1,13 @@
 class CreateShippingAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :shipping_addresses do |t|
-
+      t.string :postcode,     null: false
+      t.integer :area_id,     null: false
+      t.string :city,         null: false
+      t.string :block,        null: false
+      t.string :building
+      t.string :phone_number, null: false
+      t.references :purchase, foreign_key: true
       t.timestamps
     end
   end
