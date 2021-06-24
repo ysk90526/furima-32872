fb45da5f1c0118b456c722f5b44bc4348b3405c6
ysk90526 <y.h0526.0903@i.softbank.jp>
2021-06-24 11:58:32 +0900

    shipping_addressのバリデーションを設定

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,2 +1,13 @@
 class ShippingAddress < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :purchase
+  belongs_to :area
+  with_options presence: true do
+    validates :postcode
+    validates :city
+    validates :block
+    validates :phone_number
+  end
+  validates :area_id, with_options numericality: { other_than: 1 }
+  validates :building
 end
