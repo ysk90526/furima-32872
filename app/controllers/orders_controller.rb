798ccfd6550aa2964ba9eadb531b4de11e8a6706
ysk90526 <y.h0526.0903@i.softbank.jp>
2021-06-24 18:29:19 +0900

    paramsを作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,4 +4,9 @@ class OrdersController < ApplicationController
 
   def create
   end
+
+  private
+
+  def order_params
+    params.require(:shipping_address).permit(:postcode, :area_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id)
 end
