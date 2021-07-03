3ef4611c36d6958244a4c90fa3a98ee36a3e9f66
ysk90526 <y.h0526.0903@i.softbank.jp>
2021-07-03 18:21:00 +0900

    データを保存できるようにuser_order.rbを編集

--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -1,18 +1,19 @@
 class UserOrder
   include ActiveModel::Model
-  attr_accessor :postcode, :area_id, :city, :block, :building, :phone_number
+  attr_accessor :postcode, :area_id, :city, :block, :building, :phone_number, :token, :user_id, :item_id
 
   with_options presence: true do
     validates :postcode
     validates :city
     validates :block
     validates :phone_number
+    validates :token
   end
   validates :area_id, numericality: { other_than: 1 }
   validate :building
 
   def save
-    ShippingAddress.create(postcode: postcode, area_id: area_id, city: city, block: block, building: building, phone_number: phone_number)
-    Purchase.create(user_id: current_user.id, item_id: item.id)
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    ShippingAddress.create(postcode: postcode, area_id: area_id, city: city, block: block, building: building, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
