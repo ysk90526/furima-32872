8065143e599d7db32d3e0b35911f58c1f2a58de4
ysk90526 <y.h0526.0903@i.softbank.jp>
2021-07-03 18:21:22 +0900

    バリデーションを削除

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -2,12 +2,4 @@ class ShippingAddress < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :purchase
   belongs_to :area
-  with_options presence: true do
-    validates :postcode
-    validates :city
-    validates :block
-    validates :phone_number
-  end
-  validates :area_id, numericality: { other_than: 1 }
-  validate :building
 end
