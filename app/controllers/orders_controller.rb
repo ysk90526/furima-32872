e807c980b79d7eb8bc4a8c88685a00dc6bec1a21
ysk90526 <y.h0526.0903@i.softbank.jp>
2021-07-03 18:21:46 +0900

    payjpを追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,6 +8,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @user_order = UserOrder.new(order_params)
     if @user_order.valid?
+      pay_item
       @user_order.save
       redirect_to root_path
     else
@@ -18,6 +19,15 @@ class OrdersController < ApplicationController
   private
   
   def order_params
-    params.require(:user_order).permit(:postcode, :area_id, :city, :block, :building, :phone_number)
+    params.require(:user_order).permit(:postcode, :area_id, :city, :block, :building, :phone_number).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_7993ee0c81211f70f3c47b81"
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
   end
 end
