5de2a71591fa5422a765a147a8ff056beb5d4654
ysk90526 <y.h0526.0903@i.softbank.jp>
2021-07-07 11:04:32 +0900

    アソシエーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :items
+  has_many :purchases
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   validates :password, format: { with: VALID_PASSWORD_REGEX }
 
