c2a63c82717c43ee611ace9ec319161cb47e6f67
ysk90526 <y.h0526.0903@i.softbank.jp>
2021-07-07 11:37:23 +0900

    ログイン状態のユーザーがトップページに遷移するようにする

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,13 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
   def index
     @item = Item.find(params[:item_id])
     @user_order = UserOrder.new
+    if current_user == @item.user
+      redirect_to root_path
+    elsif @item.purchase.present?
+      redirect_to root_path
+    end
   end
 
   def create
